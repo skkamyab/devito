230d2de0bb39ef50dc577aa5d6299ce8d67d8062
Fabio Luporini <f.luporini12@imperial.ac.uk>
2018-03-19 15:21:44 +0100

    SQUASH WITH ADJUST IR TESTS

--- tests/test_ir.py
+++ tests/test_ir.py
@@ -9,6 +9,7 @@ from devito.ir.support.basic import IterationInstance, TimedAccess, Scope
 from devito.ir.support.space import (NullInterval, Interval, IntervalGroup,
                                      Any, Forward, Backward)
 from devito.ir.support.utils import detect_flow_directions
+from devito.symbolics import indexify
 
 
 @pytest.fixture(scope="session")
@@ -332,11 +333,10 @@ def test_dependences_scope(exprs, expected, ti0, ti1, ti3, fa):
 def test_flow_detection():
     """Test detection of information flow."""
     grid = Grid((10, 10))
-    u1 = TimeFunction(name="u1", grid=grid, save=10, time_order=1)
     u2 = TimeFunction(name="u2", grid=grid, time_order=2)
     u1 = TimeFunction(name="u1", grid=grid, save=10, time_order=2)
-    exprs = [LoweredEq(Eq(u1.forward, u1 + 2.0 - u1.backward)),
-             LoweredEq(Eq(u2.forward, u2 + 2*u2.backward - u1.dt2))]
+    exprs = [LoweredEq(indexify(Eq(u1.forward, u1 + 2.0 - u1.backward))),
+             LoweredEq(indexify(Eq(u2.forward, u2 + 2*u2.backward - u1.dt2)))]
     mapper = detect_flow_directions(exprs)
     assert mapper.get(grid.stepping_dim) == {Forward}
     assert mapper.get(grid.time_dim) == {Any, Forward}
